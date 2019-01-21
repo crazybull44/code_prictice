4771f557840f262265fcb1cb4d37a7c8a2c9bd9a
crazybull44 <datouisme@gmail.com>
2019-01-21 17:59:37 +0800

    个别修改
    个别修改

--- product_analysis.py
+++ product_analysis.py
@@ -5,7 +5,7 @@ import datetime as dt
 import os
 import pickle
 
-path = r'H:\quant\gtxt_data\boyangwenying3'
+path = r'E:\data1\gtxt\yuanchenhongxiang8'
 res_dict = {}
 for root, dirs, files in os.walk(path):
     for name in files:
@@ -16,11 +16,14 @@ for root, dirs, files in os.walk(path):
             continue
         df = pd.read_excel(open_file, engine='xlrd', encoding='utf-8', dtype=str)
         # df = df['证券代码']
-        df = df.loc[:, ['证券代码', '证券名称']].dropna()
+        symbol_list = df['证券代码'].tolist()
+        symbol_list.remove('nan')
         date_str = os.path.splitext(name)[0][:8]
-        res_dict[date_str] = df
+        res_dict[date_str] = symbol_list
 
 print('get res_dict success.')
-with open(r'H:\quant\gtxt_data\res_dict_bywy3.pk','wb') as f:
-    pickle.dump(res_dict,f)
-print('write data to file success.')
+# with open(r'H:\quant\gtxt_data\res_dict_bywy3_10.pk','wb') as f:
+#     pickle.dump(res_dict,f)
+# print('write data to file success.')
+print(res_dict)
+print(len(res_dict))
